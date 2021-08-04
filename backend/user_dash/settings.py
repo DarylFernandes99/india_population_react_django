f48722ebec1ad24f7e3f151c1833431fc90884ac
Daryl Fernandes <36784577+DarylFernandes99@users.noreply.github.com>
2021-08-04 12:09:54 +0530

    Update settings.py

--- backend/user_dash/settings.py
+++ backend/user_dash/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+*6r-qs$^9z1q(8ne2(h5l(vhoks=0w#b7mllk#wgufa5d^2!@'
+SECRET_KEY = 'insert secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
